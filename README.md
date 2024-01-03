e3f7a0f964d302a292be0e2c0b951b5ff6ac6072
Aleah White <aleahwhite55@gmail.com>
2024-01-03 04:20:30 -0600

    added apis to js, styled in css

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Challenge 6 - Weather Dashboard
+
+## Description:
+
+
+## Image and Link:
+
+Link to deployed website: 
+
+## Installation:
+N/A
+
+## Usage:
+
+
+## Credits:
+N/A
+
+## License:
+Please refer to the MIT License in this repository!
---
+++ assets/script.js
@@ -0,0 +1,97 @@
+var apiKey = '640ea64248ec5c311fdebf8bdc7e577b';
+var button = document.getElementById('search-button');
+var locationInput = document.getElementById('location-input');
+
+locationInput.addEventListener('keypress', function (event) {
+    if (event.keyCode === 13) { // 13 is the keycode for 'return'
+        performSearch();
+    }
+});
+
+function createWeatherCard(item, index) {
+    var container = document.getElementById('weather-card-container');
+
+    var card = document.createElement('div');
+    card.className = 'weather-card';
+
+    var title = document.createElement('h3');
+    title.textContent = 'Day ' + (index + 1);
+
+    var temp = document.createElement('p');
+    temp.textContent = 'Temperature: ' + item.main.temp + ' K'; //change to F once you add the JS to convert K to F
+
+    var humidity = document.createElement('p');
+    humidity.textContent = 'Humidity: ' + item.main.humidity + ' %rh';
+
+    card.appendChild(title);
+    card.appendChild(temp);
+    card.appendChild(humidity);
+
+    container.appendChild(card);
+
+}
+
+
+button.addEventListener('click', performSearch);
+
+function performSearch() {
+
+    var location = document.getElementById('location-input').value;
+    var apigeoURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + location + '&limit=1&appid=' + apiKey;
+
+    fetch(apigeoURL, {
+        method: 'GET'
+    })
+
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+
+
+            for (var i = 0; i < data.length; i++) {
+
+                var lat = data[i].lat;
+                var lon = data[i].lon;
+
+                console.log('City: ' + data[i].name);
+                console.log('Latitude: ' + data[i].lat);
+                console.log('Longitude: ' + data[i].lon);
+
+
+                var apiURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
+
+                fetch(apiURL, {
+                    method: 'GET'
+                })
+
+                    .then(function (response) {
+                        return response.json();
+                    })
+
+                    .then(function (weatherData) {
+                        if (weatherData && weatherData.list) {
+
+                            weatherData.list.slice(0, 6).forEach(function (item, index) {
+                                if (item.main) {
+                                    createWeatherCard(item, index);
+                                }
+                            });
+
+                            for (let i = 0; i < 6; i++) {
+                                if (i < weatherData.list.length && weatherData.list[i].main) {
+                                    console.log('Item ' + i + ':  Main:', weatherData.list[i].main);
+                                }
+                            }
+                        }
+                    });
+
+            }
+        });
+
+};
+
+
+
